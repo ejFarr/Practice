e236581700acdeb4c9269b4bd137068eb0f6dc8a
Evan Farr <roboproductions200@gmail.com>
2019-08-30 12:53:55 -0600

    Initial commit

---
+++ Timeuntil.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.time.LocalDateTime;
+
+public class Timeuntil
+{
+    public static void main (String[] args)
+    {
+        Scanner scam = new Scanner(System.in);
+        int h = LocalDateTime.now().getHour();
+        int m = LocalDateTime.now().getMinute();
+        int s = LocalDateTime.now().getSecond();
+
+        System.out.println("Input the hour: ");
+        int nh = scam.nextInt();
+
+        System.out.println("Input the minutes: ");
+        int nm = scam.nextInt();
+
+        System.out.println("Input the second(s): ");
+        int ns = scam.nextInt();
+
+
+        int d = nh - h;
+        int md = nm - m;
+        int sd = ns - s;
+
+        if (md < 0)
+        {
+            d -= 1;
+            md += 60;
+        }
+        if (sd < 0)
+        {
+            md -= 1;
+            sd += 60;
+        }
+        if (d > 0)
+        {
+            System.out.println("You are " + d + " hours away");
+        }
+        if (md > 0)
+        {
+            System.out.println("You are " + md + " minutes away");    
+        }
+        if (sd > 0)
+        {
+            System.out.println("You are " + sd + " seconds away");    
+        }
+        
+        
+
+    }
+}

45652618f451c4dd9c49af7193d0b4dfb9893fca
Evan Farr <roboproductions200@gmail.com>
2019-09-03 12:53:14 -0600

    signup

---
+++ Signup.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Signup
+{
+    public static void main(String[] args) 
+    {
+        boolean gpass;
+        boolean guser;
+        Scanner scram = new Scanner(System.in);
+        System.out.println("Enter Username: ");
+        String user = scram.nextLine();
+        System.out.println("Enter Password: ");
+        String pass = scram.nextLine();
+
+        if (user.equals("person"))
+        {
+            System.out.println("Username is not available");
+        }
+        if (user.equals("admin"))
+        {
+            System.out.println("Username is not available");
+        }
+        if (user.equals("jimbob234"))
+        {
+            System.out.println("Username is not available");
+        }
+        if (user.equals("skater_gurl17"))
+        {
+            System.out.println("Username is not available");
+        }
+        if (user.equals("gary"))
+        {
+            System.out.println("Username is not available");
+        }
+        if ()
+        {
+            
+        }
+
+    }
+}

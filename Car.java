81898c82cff676e3357a6065ae8e8dd57fdc643c
Evan Farr <roboproductions200@gmail.com>
2019-10-23 12:27:22 -0600

    finishes Fjord

--- Car.java
+++ Car.java
@@ -1,12 +1,36 @@
 public class Car {
-    String model = "";
-    String type = "";
-    int tankSize = 0;
-    double time = 0;
+    private String model;
+    private String type;
+    private double tankSize;
+    private double time;
+    private double mpg;
+
     public Car(String model, String type, int tankSize, double time) {
         this.model = model;
         this.type = type;
-        this.tankSize = tankSize;
+        this.tankSize = literToGallon(tankSize);
         this.time = time;
+        this.mpg = calculateMPG();
+    }
+
+    private double calculateMPG() {
+        return (time * 60)/tankSize;
+    }
+
+    private boolean meetsStandards() {
+        if (type.equals("Sedan")) return mpg >= 50;
+        if (type.equals("SUV")) return mpg >= 40;
+        if (type.equals("Truck")) return mpg >= 30;
+
+        System.out.println(type + " is not a car type.");
+        return false;
+    }
+
+    private double literToGallon(double tankSize) {return tankSize / 3.785;}
+
+    public void printResults() {
+        if (meetsStandards()) System.out.println(model + ": MPG: " + mpg + ": Does meet standards");
+        else System.out.println(model + ": MPG: " + mpg + ": Does not meet standards");
+
     }
 }
